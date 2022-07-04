2a84894abfaeb16b9490449af016d8f2a98db4b6
Mantas <m.juozenas@gmail.com>
2022-07-04 09:29:21 +0300

    authContext, app.js routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import HomePage from './page/Home/HomePage';
+import LoginPage from './page/Login/LoginPage';
+import RegisterPage from './page/Register/RegisterPage';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello again world</h1>
+      <Switch>
+        <Route exact path='/'>
+          <HomePage />
+        </Route>
+        <Route path='/login'>
+          <LoginPage />
+        </Route>
+        <Route path='/register'>
+          <RegisterPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
